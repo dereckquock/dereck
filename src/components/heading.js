972d96d2532218e468a7ae2beacb43d3ef0b11bf
dereck <dquock@paypal.com>
2020-05-19 01:54:13 -0700

    add wave animation

--- src/components/heading.js
+++ src/components/heading.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { keyframes } from '@emotion/core';
 import Avatar from '../components/avatar';
+
+const wave = keyframes`
+  0% { transform: rotateZ(0) }
+  25% { transform: rotateZ(-10deg) }
+  50% { transform: rotateZ(10deg) }
+  75% { transform: rotateZ(-10deg) }
+  100% { transform: rotateZ(0) }
+`;
 
 function Heading() {
   return (
     <h1 css={{ display: 'flex', alignItems: 'center' }}>
       <Avatar />
-      <span css={{ marginLeft: '1.35rem' }}>{`Hi 👋 I'm Dereck Quock`}</span>
+      <span css={{ marginLeft: '1.35rem' }}>
+        Hi{' '}
+        <span
+          css={{
+            display: 'inline-block',
+            transformOrigin: 'right bottom',
+            animation: `${wave} 1s 0.5s 1 normal ease-out`,
+          }}
+        >
+          👋
+        </span>{' '}
+        I&apos;m Dereck Quock
+      </span>
     </h1>
   );
 }
